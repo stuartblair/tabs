eac71e6ffd9268d1d4c7c2fa2bff426d03b178b5
Stuart Blair <stuart.blair@ge.com>
2017-08-09 16:33:34 -0700

    Adding check to make sure we're building with the right version of Node

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,24 @@
-(function() {
+(function () {
     "use strict";
 
+    let EXPECTED_NODE_VERSION = "v7.4.0"
 
     desc("This is the default build")
-    task("default", function() {
+    task("default", function () {
         console.log("\n\nBUILD OK");
     });
-    
+
+    desc("Check Node version");
+    task("version", function () {
+        let actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Incorrect version of Node: expected " +
+                EXPECTED_NODE_VERSION +
+                ", but found version " +
+                actualVersion
+            );
+        }
+        console.log("Expected Node version found: " + EXPECTED_NODE_VERSION);
+    });
+
 }());
