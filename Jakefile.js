22f789db31f5f4446cc41d783c6ffe83246f5463
Stuart Blair <stuart.blair@ge.com>
2017-08-09 11:29:09 -0700

    Moving to a Jakefile

---
+++ Jakefile.js
@@ -0,0 +1,10 @@
+(function() {
+    "use strict";
+
+
+    desc("This is the default build")
+    task("default", function() {
+        console.log("\n\nBUILD OK");
+    });
+    
+}());
---
+++ README.md
@@ -0,0 +1,3 @@
+* Building
+npm install jake -g
+Jake
