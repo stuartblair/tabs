4ccbb81bcc978aad5573bebdf7df738a5c1d5357
Stuart A Blair <stuart.a.blair@gmail.com>
2017-08-09 22:10:12 -0700

    Linting by default

--- Jakefile.js
+++ Jakefile.js
@@ -2,6 +2,7 @@
     "use strict";
 
     var semver = require("semver");
+    var jshint = require("simplebuild-jshint");
 
     desc("This is the default build");
     task("default", ["version", "lint"], function () {
@@ -25,8 +26,12 @@
 
     desc("Lint JavaScript code");
     task("lint", function() {
-        console.log("Linting JavaScript: .");
+        process.stdout.write("Linting JavaScript: ");
 
-        jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", { interactive: true}, complete);
+        jshint.checkFiles({
+            files: "Jakefile.js",
+            options: {},
+            globals: {} 
+        }, complete, fail);
     }, { async: true });
 }());
