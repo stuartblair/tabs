8268c6cdc1562c8464975643c52aa8c91a7f6f7d
Stuart A Blair <stuart.a.blair@gmail.com>
2017-08-09 21:56:22 -0700

    Added JSHint

--- Jakefile.js
+++ Jakefile.js
@@ -3,15 +3,15 @@
 
     var semver = require("semver");
 
-    desc("This is the default build")
-    task("default", ["version"], function () {
+    desc("This is the default build");
+    task("default", ["version", "lint"], function () {
         console.log("\n\nBUILD OK");
     });
 
     desc("Check Node version");
     task("version", function () {
-        var expectedVersion = "6.11.2"
-        var packageJson = require("./package.json")
+        var expectedVersion = "6.11.2";
+        var packageJson = require("./package.json");
         var actualVersion = packageJson.engines.node;
         if (semver.neq(actualVersion, expectedVersion)) {
             fail("Incorrect version of Node: expected " +
@@ -23,4 +23,10 @@
         console.log("Expected Node version found: " + expectedVersion);
     });
 
+    desc("Lint JavaScript code");
+    task("lint", function() {
+        console.log("Linting JavaScript: .");
+
+        jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", { interactive: true}, complete);
+    }, { async: true });
 }());
