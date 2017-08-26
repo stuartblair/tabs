d6fd763db7696a6674b1b05a27bcbe6044340978
Stuart A Blair <stuart.a.blair@gmail.com>
2017-08-25 22:39:27 -0700

    Amending Jakefile to reflect change in formatting for quotes

--- Jakefile.js
+++ Jakefile.js
@@ -1,41 +1,41 @@
 /* global jake, complete, desc, task, console, fail */
 (function () {
-    "use strict";
+    'use strict';
 
-    var semver = require("semver");
+    var semver = require('semver');
 
     /* General tasks */
 
-    desc("This is the default build");
-    task("default", ["version", "lint"], function () {
-        console.log("\n\nBUILD OK");
+    desc('This is the default build');
+    task('default', ['version', 'lint'], function () {
+        console.log('\n\nBUILD OK');
     });
 
-    desc("Run a localhost server");
-    task("run", function() {
-        console.log("Run http server");
-        jake.exec("node node_modules/http-server/bin/http-server src", { interactive: true }, complete);
+    desc('Run a localhost server');
+    task('run', function() {
+        console.log('Run http server');
+        jake.exec('node node_modules/http-server/bin/http-server src', { interactive: true }, complete);
     });
 
     /* Supporting tasks */
 
-    desc("Check Node version");
-    task("version", function () {
-        var expectedVersion = "6.11.2";
-        var packageJson = require("./package.json");
+    desc('Check Node version');
+    task('version', function () {
+        var expectedVersion = '6.11.2';
+        var packageJson = require('./package.json');
         var actualVersion = packageJson.engines.node;
         if (semver.neq(actualVersion, expectedVersion)) {
-            fail("Incorrect version of Node: expected " +
+            fail('Incorrect version of Node: expected ' +
                 expectedVersion +
-                ", but found version " +
+                ', but found version ' +
                 actualVersion
             );
         }
     });
 
-    desc("Lint JavaScript code");
-    task("lint", function () {
-        process.stdout.write("Linting JavaScript: ");
-        jake.exec("node node_modules/.bin/eslint \"**/*.js\"", {interactive: true}, complete);
+    desc('Lint JavaScript code');
+    task('lint', function () {
+        process.stdout.write('Linting JavaScript: ');
+        jake.exec('node node_modules/.bin/eslint \'**/*.js\'', {interactive: true}, complete);
     }, { async: true });
 }());
